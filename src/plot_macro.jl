10efa794970be7e57a0c91207aa1b5ee1b00282a
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-11-28 18:37:48 +0000

    moved to new indexedtables

--- src/plot_macro.jl
+++ src/plot_macro.jl
@@ -11,29 +11,28 @@ end
 function plot_helper(t::ProcessedTable, f::Function, err_style, args...; kwargs...)
     s = t.table
     axis_type = t.kw[:axis_type]
-    group = s.index.columns[1:end-1]
+    group = Tuple(columns(t.table, s) for s in listsplits(t))
     group_list = unique(Columns(group...))
     plot_kwargs = [(plot_kwarg[1], reshape(plot_kwarg[2].(group_list), 1, :))
         for plot_kwarg in t.kw[:plot_kwargs]]
     if axis_type == :pointbypoint
         err_kwargs = []
-        if eltype(s.data.columns[1])<:Tuple
-            x = getindex.(s.data.columns[1], 1)
-            push!(err_kwargs, (:xerr, getindex.(s.data.columns[1], 2)))
+        if eltype(columns(s, :x))<:Tuple
+            x = getindex.(columns(s, :x), 1)
+            push!(err_kwargs, (:xerr, getindex.(columns(s, :x), 2)))
         else
-            x = s.data.columns[1]
+            x = columns(s, :x)
         end
-        if eltype(s.data.columns[2])<:Tuple
-            y = getindex.(s.data.columns[2], 1)
-            push!(err_kwargs, (:yerr, getindex.(s.data.columns[2], 2)))
+        if eltype(columns(s, :y))<:Tuple
+            y = getindex.(columns(s, :y), 1)
+            push!(err_kwargs, (:yerr, getindex.(columns(s, :y), 2)))
         else
-            y = s.data.columns[2]
+            y = columns(s, :y)
         end
         f(args..., x, y; group = group, err_kwargs...,kwargs..., plot_kwargs...)
     else
-        x = s.index.columns[end]
-        y = isa(s.data, Columns) ? s.data.columns[1] : s.data
-        err = (isa(s.data, Columns) && (length(s.data.columns) > 1)) ? s.data.columns[2] : nothing
+        x, y = columns(s, :x), columns(s, :y)
+        err = :err in colnames(s) ? columns(s, :err) : nothing
         (axis_type == :continuous) && (err_style = :ribbon)
         f(args..., x, y; group = group, (err_style, err), kwargs..., plot_kwargs...)
     end
